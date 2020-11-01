e0a44ccffa92a71cc175c6e094853bbdcebd7407
Elías Huerta <elias.huerta@icloud.com>
2020-11-01 16:58:03 +0100

    Services Post and User

---
+++ src/app/post.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UserService } from './user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  private apiUrl: string;
+  private httpOptions: object;
+  constructor(private httpClient: HttpClient, private userService: UserService) {
+    this.apiUrl = 'http://127.0.0.1:8000/api/posts';
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer' + this.userService.getToken()
+      })
+    }
+  };
+
+  getPosts() {
+    return this.httpClient.get(this.apiUrl, this.httpOptions).toPromise();
+  };
+
+  addPost(pData) {
+    return this.httpClient.post(this.apiUrl, pData, this.httpOptions).toPromise();
+  };
+
+  getOnlyPost(pId) {
+    return this.httpClient.get(this.apiUrl + '/' + pId, this.httpOptions).toPromise();
+  };
+
+  updatePost(pId, pData) {
+    return this.httpClient.put(this.apiUrl + '/' + pId, pData, this.httpOptions).toPromise();
+  };
+
+  deletePost(pId) {
+    return this.httpClient.put(this.apiUrl + '/' + pId, this.httpOptions).toPromise();
+  };
+
+}
---
+++ src/app/user.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private registerURL: string;
+  private loginURL: string;
+
+  constructor(private httpClient: HttpClient) {
+    this.loginURL = 'http://127.0.0.1:8000/register';
+    this.registerURL = 'http://127.0.0.1:8000/api/login_check'
+  };
+
+  registerUser(pUser) {
+    return this.httpClient.post(this.registerURL, pUser).toPromise();
+  };
+
+  loginUser(pCredentials) {
+    return this.httpClient.post(this.loginURL, pCredentials).toPromise();
+  };
+
+  setToken(token) {
+    localStorage.setItem('token_user', token);
+  }
+
+  getToken() {
+    localStorage.getItem('token_user');
+  };
+}
